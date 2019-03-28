8a447bad7b1847fc967d74f6ba62fcaf6ddf6cf9
name4less <nastasiya.nimchenko@gmail.com>
2019-03-28 12:13:25 +0200

    ädd indexF2.js

---
+++ indexF2.js
@@ -0,0 +1,164 @@
+/* # Домашнее задание. Функции 2, ES6
+
+## ES6
+Переделайте предыдущее ДЗ используя максимум возможностей **ES6**. Отметьте облегчение (или утяжеление) синтаксиса.
+
+## sort TRY DONE
+Сделайте обобщенную функцию сортировки массива
+
+```javascript
+var persons = [
+    {name: "Иван", age: 17},
+    {name: "Мария", age: 35},
+    {name: "Алексей", age: 73},
+    {name: "Яков", age: 12},
+]
+
+sort(persons, "age"); //сортирует по возрасту по возрастанию
+sort(persons, "name", false); //сортирует по имени по убыванию
+```
+
+Функция позволяет отсортировать *любой набор* данных по имени поля (второй параметр). Третьим параметром идет 
+необязательный `Boolean`, который в случае `true` делает сортировку по возрастанию, в случае `false` - по убыванию. 
+По умолчанию (без третьего параметра) происходит сортировка по возрастанию.
+
+## array map DONE
+Используя `Array.map` приведите все строки в массиве к числу. Элементы других типов оставьте как есть:
+```javascript
+["1", {}, null, undefined, "500", 700]
+```должно превратиться в 
+```javascript
+[1, {}, null, undefined, 500, 700]
+```
+
+## array reduce DONE
+Получите произведение всех чисел в массиве, используя `Array.reduce`. Не обрабатывайте типы данных, не являющиеся числом.
+```javascript
+["0", 5, 3, "string", null]
+```
+результат должен быть 15
+
+
+## object filter DONE
+Напишите свою реализацию `Array.filter` для **объектов**:
+
+```javascript
+var phone = {
+    brand: "meizu",
+    model: "m2",
+    ram: 2,
+    color: "black",
+};
+filter(phone,(key,value) => key == "color" || value == 2);
+```
+должно вернуть 
+```javascript
+{
+    ram: 2,
+    color: "black",
+}
+```
+Для удаления пары ключ-значение используйте `delete`. Или сделайте *копию* объекта.
+
+## object map DONE
+Напишите свою реализацию `Array.map` для **объектов**:
+```javascript
+map({name: "Иван", age: 17},function(key,value){
+    var result = {};
+    result[key+"_"] = value + "$";
+    return result;
+}) //должен вернуть {name_: "Иван$", age_: "17$"}
+```*/
+
+//## sort 
+var persons = [
+    {name: "Иван", age: 17},
+    {name: "Мария", age: 35},
+    {name: "Алексей", age: 73},
+    {name: "Яков", age: 12},
+];
+
+function sortPublic(obj, nameParam, boolParam){
+    function sortAscending(paramA, paramB){
+        return (paramA[nameParam] < paramB[nameParam]) ? -1 : 1;
+    };
+
+    function sortDescending(paramA, paramB){
+        return (paramA[nameParam] > paramB[nameParam]) ? -1 : 1;
+    };
+
+    if((boolParam==null)||(boolParam==true)){
+        obj.sort(sortAscending);
+    }
+    else if(boolParam==false){
+        obj.sort(sortDescending);
+    }
+    else{
+        alert('Wrong third parameter');
+    }
+};
+
+sortPublic(persons, name, true);
+
+//output persons
+for(var i = 0; i < persons.length; i++) {
+    alert('Name - '+persons[i].name+' age - '+persons[i].age);
+  };
+
+//## array map
+var arrayOrigin = ["1", {}, null, undefined, "500", 700];
+var arrayToNumber = arrayOrigin.map(function(key){
+    if((typeof key) =='string'){
+        return +key;
+    }
+    else{
+        return key;
+    }
+});
+
+//rider array
+arrayToNumber.forEach(function(item, i, arrayToNumber){
+    alert(i+":"+item+"(array:"+arrayToNumber+")");
+});
+
+//## array reduce 
+var composition = arrayOrigin.reduce(function(comp=1, key){
+    if(typeof key=='number'){
+        return comp*key;
+    };
+});
+alert(composition);
+
+//## object map
+var objOrigin = {name: "Иван", age: 17};
+
+var objMap = Object.keys(objOrigin).map(function(key,index){
+    var result={};
+    result[key+'_']= objOrigin[key]+'$';
+    return result;
+});
+
+Object.keys(objMap).map(function(key,index){
+    var result={};
+    result[key+'_']= objOrigin[key]+'$';
+    return result;
+});
+
+console.log(objMap);
+
+//## object filter
+
+Object.fromEntries = arr => Object.assign({}, ...arr.map( ([k, v]) => ({[k]: v}) ));
+    Object.filter = (obj, predicate) => 
+            Object.fromEntries(Object.entries(obj).filter(predicate));
+          
+// Example use:
+var phone = {
+    brand: "meizu",
+    model: "m2",
+    ram: 2,
+    color: "black",
+};
+
+var filtered = Object.filter(phone, ([key, value]) => (key == 'color' || value == 2)); 
+console.log(filtered);
